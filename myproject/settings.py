e8d7d264bdfa20c4380a16ade7b04451c405cad2
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-12-02 03:00:16 +0530

    updated: settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1']
 
 # MongoDB configuration
 MONGO_URI = config('MONGO_URI')
