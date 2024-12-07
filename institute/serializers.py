f009c6a4416fd785a23b46fcbb8ba2b26aa0b995
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-12-08 01:30:02 +0530

    validation: message for bad institute request

--- institute/serializers.py
+++ institute/serializers.py
@@ -5,3 +5,12 @@ class InstituteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Institute
         fields = '__all__'
+    
+    def to_internal_value(self, data):
+        required_fields = [field.name for field in Institute._meta.fields if not field.null and not field.blank]
+        
+        for field in required_fields:
+            if field not in data:
+                raise serializers.ValidationError({"message": "Insufficient data"})
+        
+        return super().to_internal_value(data)
