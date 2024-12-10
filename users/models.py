5e6e7815d554179d31d4701a1368c1b4d549e100
Ratan Deep Singh <ratandeep.blr.12@gmail.com>
2024-12-11 02:02:42 +0530

    fixed: onboarding and approval

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     otp = models.CharField(max_length=6, null=True, blank=True)
     is_approved = models.BooleanField(default=False)
+    onboarding = models.IntegerField(default=0)
     role = models.CharField(
         max_length=20,
         null=True,
