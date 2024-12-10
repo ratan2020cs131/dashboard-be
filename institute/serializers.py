5a70dcae1f028643d78aa71de5639f5ccb1de267
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-12-11 02:09:26 +0530

    fixed: onboarding and approval (#10)

--- institute/serializers.py
+++ institute/serializers.py
@@ -4,20 +4,29 @@ from users.serializers import UserSerializer
 
 class InstituteSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
-    institute_code = serializers.CharField(
-        validators=[]
-    )
+    institute_code = serializers.CharField(validators=[])
     
     class Meta:
         model = Institute
         fields = ['id', 'institute_code', 'institute_name', 'institute_city', 'user']
     
     def to_internal_value(self, data):
+        # Check for missing fields
         required_fields = [field.name for field in Institute._meta.fields if not field.null and not field.blank]
-        for field in required_fields:
-            if field not in data:
-                raise serializers.ValidationError({"message": "Insufficient data"})
+        missing_fields = [field for field in required_fields if field not in data or not data.get(field)]
+        if missing_fields:
+            raise serializers.ValidationError({"message": "Insufficient data"})
+        
+        # Check for unique institute code
         institute_code = data.get('institute_code')
         if Institute.objects.filter(institute_code=institute_code).exists():
-            raise serializers.ValidationError({"message": "institute with this institute code already exists"})
+            raise serializers.ValidationError({"message": "Institute with this institute code already exists"})
+        
         return super().to_internal_value(data)
+    
+    def validate(self, data):
+        # Override to handle blank fields
+        for field in ['institute_name', 'institute_city', 'institute_code']:
+            if not data.get(field):
+                raise serializers.ValidationError({"message": "Insufficient data"})
+        return data
--- users/services.py
+++ users/services.py
@@ -7,6 +7,7 @@ def update_user_role(user_id, new_role):
        try:
               user = User.objects.get(id=user_id)
               user.role = new_role
+              user.onboarding=1
               user.save()
               return user
        except User.DoesNotExist:
